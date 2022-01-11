99df251ea58833f826142af2a339d5576c828c18
marysol <marilirulita@gmail.com>
2022-01-11 11:51:06 -0800

    Add setup webpack fies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  element.classList.add('hello');
+  
   return element;
 }
 
