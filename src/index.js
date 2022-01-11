d6d45d37d7695ec995292b0a506d7a1155f38b98
marysol <marilirulita@gmail.com>
2022-01-11 12:44:53 -0800

    Add array of object and function to dislay array elements

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+let list = [
+  {
+    description: 'Clean Room',
+    completed: false,
+    index: 2
+  },
+  {
+    description: 'Wash Dishes',
+    completed: false,
+    index: 4
+  },
+  {
+    description: 'Make Dinner',
+    completed: false,
+    index: 3
+  },
+  {
+    description: 'first but last',
+    completed: false,
+    index: 1
+  }
+];
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  
-  return element;
+function display() {
+  list.sort((x, y) => {
+    return x.index - y.index;
+  });
+
+  const listElements = document.createElement('ul');
+
+  list.forEach(element => {
+    const elementList = document.createElement('li');
+    const description = document.createElement('span');
+    description.innerHTML = element.description; 
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+
+    elementList.classList.add('elementList');
+    elementList.appendChild(checkBox);
+    elementList.appendChild(description);
+    listElements.appendChild(elementList);
+  })
+  const listPlacehold = document.getElementById('list-placeholder');
+  listPlacehold.appendChild(listElements);
+  console.log(listElements);
 }
 
-document.body.appendChild(component());
+display();
