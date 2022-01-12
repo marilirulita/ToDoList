1b6f5b9d6f3f163ce0fa1005cca89593b6a7115a
marysol <marilirulita@gmail.com>
2022-01-12 11:27:20 -0800

    add update index function

--- src/deleteElements.js
+++ src/deleteElements.js
@@ -1,17 +1,23 @@
 import display from './index.js';
 
 const deleteComplete = (list) => {
-      
+  const newList = list.filter((task) => task.completed === false);
+  console.log(newList);
+  list = newList;
+  updatePosition(list);
+  display(list);
+}
   //     const local = window.localStorage.getItem('tasklist');
   //     const list = JSON.parse(local);
-       const newList = list.filter((task) => task.completed === false);
-       console.log(newList);
-       list = newList;
-       display(list);
-   //    updatePosition(newList);
+        //    updatePosition(newList);
     //   showItems(newList);
     //   saveList(newList);
-    return list;
-     }
+     
+const updatePosition = (newList) => {
+  newList.forEach((task, id) => {
+    console.log(id);
+    task.index = id;
+  });
+}
 
 export {deleteComplete as default};
