43ba3941389eb03f3a59b080aa867c88decd3432
marysol <marilirulita@gmail.com>
2022-01-13 17:32:37 -0800

    Add drag and drop function

--- src/index.js
+++ src/index.js
@@ -7,5 +7,32 @@ window.onload = () => {
   if (local != null) {
     list = JSON.parse(local);
   }
-  display(list);
+  // display(list);
 };
+
+const dragElementAll = document.querySelectorAll('.dragElements');
+for (const dragElement of dragElementAll) {
+  dragElement.addEventListener('dragstart', e => {
+    e.dataTransfer.setData('text/plain', dragElement.id);
+   // dragElement.classList.add('hiden');
+  });
+}
+
+const dropZone = document.querySelector('.drop-zone');
+dropZone.addEventListener('dragover', e => {
+  e.preventDefault();
+  dropZone.classList.add('drop-zone-over');
+});
+
+dropZone.addEventListener('dragleave', () => {
+  dropZone.classList.remove('drop-zone-over');
+});
+
+dropZone.addEventListener('drop', e => {
+  e.preventDefault();
+  const dropedElementId = e.dataTransfer.getData('text/plain');
+  const dropedElement = document.getElementById(dropedElementId);
+  dropZone.appendChild(dropedElement);
+  dropZone.classList.remove('drop-zone-over');
+  
+});
