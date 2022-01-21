7710843743e739457d30f60f304e5a2beb614895
Jose Herrera <Heco@Alejandras-MacBook-Pro.local>
2022-01-21 18:02:14 -0500

    add true function and delete one task

--- src/__mocks__/removeCompleted.js
+++ src/__mocks__/removeCompleted.js
@@ -7,9 +7,29 @@ const checkStatus = (element, checkBox, span) => {
   }
 };
 
+class CheckLocal {
+  constructor() {
+    if (localStorage.getItem('list') === null) {
+      this.list = [];
+    } else {
+      this.list = JSON.parse(localStorage.getItem('list'));
+    }
+  }
+
+  saveStorage = () => {
+    localStorage.setItem('list', JSON.stringify(this.list));
+  }
+
+  doneTask = (index) => {
+    const task = this.list[index];
+    task.completed = true;
+    this.saveStorage();
+  }
+}
+
 const completed = (list) => {
   const newList = list.filter((task) => task.completed === false);
   return newList;
 };
 
-export { completed, checkStatus };
+export { completed, checkStatus, CheckLocal };
--- src/__test__/edit.test.js
+++ src/__test__/edit.test.js
@@ -1,19 +1,41 @@
-import { addItem } from '../__mocks__/display.js';
+import { CheckLocal, completed, checkStatus } from '../__mocks__/removeCompleted.js';
+
+const newList = new CheckLocal();
 
 const list = [
   {
-    description: 'Finish part 2',
+    description: 'Finish project Microverse today',
     completed: true,
     index: 0,
   },
   {
-    description: 'Meeting at 8 am',
+    description: 'Meeting at 5 pm',
     completed: false,
     index: 1,
   },
   {
-    description: 'Hope approved at first strike',
+    description: 'Go to the supermarket',
     completed: false,
     index: 2,
   },
 ];
+
+newList.list[0] = { description: 'Finish part 2', completed: false, index: 0 };
+newList.list[1] = { description: 'Meeting at 8 am', completed: false, index: 1 };
+newList.list[2] = { description: 'Hope approved at first strike', completed: false, index: 2 };
+
+// test('true', () => {
+//   const checked = document.getElementById('0');
+//   checkStatus(list[0], list);
+//   expect(list[0].complete).toBe(true);
+// });
+
+test('true', () => {
+  newList.doneTask(0);
+  expect(newList.list[0].completed).toBe(true);
+});
+
+test('igual', () => {
+  const listNew = completed(list);
+  expect(listNew).toHaveLength(2);
+});
