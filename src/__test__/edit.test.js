c8ad2b28b2b294f00a3d6f09fa56d2a8ee7cf53d
marysol <marilirulita@gmail.com>
2022-01-21 07:29:37 -0800

    Create new file for test

---
+++ src/__test__/edit.test.js
@@ -0,0 +1,19 @@
+import { addItem } from '../__mocks__/display.js';
+
+const list = [
+  {
+    description: 'Finish part 2',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'Meeting at 8 am',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Hope approved at first strike',
+    completed: false,
+    index: 2,
+  },
+];
